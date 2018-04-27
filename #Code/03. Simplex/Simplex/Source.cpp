0c71963ddea8496d52d73b74305b552eb5cff5e9
LiuPeng-PC-3 <2224214496@qq.com>
2018-04-27 23:57:29 +0800

    Fucking Microsoft, sometimes run, sometimes crash!

--- #Code/03. Simplex/Simplex/Source.cpp
+++ #Code/03. Simplex/Simplex/Source.cpp
@@ -15,7 +15,9 @@ email: littleNewton6@outlook.com
 #include<stdlib.h>
 #include<math.h>
 
-typedef struct Matrix{
+typedef struct Matrix {
+    // The INDEX begin with 1 rather 0 is reasonable.
+    // TRY make this program behave like MATLAB linprog.
     double *low_level_array;
     int n_row;
     int n_column;
@@ -24,7 +26,7 @@ typedef struct Matrix{
 Matrix *Matrix_init(int n_row, int n_column) {
     // This is a constructor for Matrix data type.
     Matrix *ans = (Matrix *)calloc(1, sizeof(Matrix));
-    ans->low_level_array = (double *)calloc(sizeof(double), n_row * n_column);
+    ans->low_level_array = (double *)calloc(n_row * n_column, sizeof(double));
     ans->n_column = n_column;
     ans->n_row = n_row;
     return ans;
@@ -32,20 +34,20 @@ Matrix *Matrix_init(int n_row, int n_column) {
 
 Matrix *Matrix_copy(Matrix *m) {
     // Generate a deep copy of Matrix m.
-    Matrix *ans = Matrix_init(m->n_row, m->n_row);
+    Matrix *ans = Matrix_init(m->n_row, m->n_column);
     int i = 0;
     int j = 0;
     for (; i < m->n_row; i++) {
         for (; j < m->n_column; j++) {
-            *(ans + i * m->n_column + j) = \
+            *(ans + i * ans->n_column + j) = \
                 *(m + i * m->n_column + j);
         }
     }
     return ans;
 }
 
-double Matrix_Element_get(Matrix *m, int i, int j) {
-    return *(m->low_level_array + (i - 1) * m->n_column + j - 1);
+double Get_Matrix_Element(Matrix *m, int i, int j) {
+    return *(m->low_level_array + (i - 1) * m->n_column + (j - 1));
 }
 
 Matrix *number_mul_vector(double N, int n_row, Matrix *m) {
@@ -77,7 +79,7 @@ Matrix *pivot_Element_Trans(Matrix *m, int pivot_row, int pivot_column) {
     // trans into zero. Value checking is needed in this function.
     Matrix *ans = Matrix_copy(m);
     double pivot = 0;
-    pivot = Matrix_Element_get(m, pivot_row, pivot_column);
+    pivot = Get_Matrix_Element(m, pivot_row, pivot_column);
 
     // Value Checking
     if (pivot == 0) {
@@ -87,12 +89,12 @@ Matrix *pivot_Element_Trans(Matrix *m, int pivot_row, int pivot_column) {
 
     int i = 1;
     for (; i <= ans->n_row; i++) {
-        double adjacent_Element = Matrix_Element_get(ans, i, pivot_column);
+        double adjacent_Element = Get_Matrix_Element(ans, i, pivot_column);
         if (i != pivot_row && adjacent_Element != 0) {
             ans = number_mul_vector(-1 * adjacent_Element / pivot, pivot_row, ans);
             // Modify the Matrix to fit this row.
             ans = vector_add_vector(ans, i, pivot_row);
-            pivot = Matrix_Element_get(m, pivot_row, pivot_column);
+            pivot = Get_Matrix_Element(m, pivot_row, pivot_column);
             ans = number_mul_vector(1 / pivot, pivot_row, ans);
         }
     }
@@ -105,26 +107,26 @@ void Matrix_print(Matrix *matrix) {
     int j;
     for (i = 0; i < matrix->n_row; i++) {
         for (j = 0; j < matrix->n_column; j++) {
-            printf("%2.2f\t", Matrix_Element_get(matrix, i+1, j+1));
+            printf("%2.2f\t", Get_Matrix_Element(matrix, i + 1, j + 1));
         }
         printf("\n");
     }
 }
 
-typedef struct Simplex_Tab{
+typedef struct Simplex_Tab {
     Matrix *simplex_table;
-    
+
 }Simplex_Tab;
 
 int main(int argc, int *argv[]) {
     /*
-    EXAMPLE: 
-        Power Shell> Simplex "(c1, c2, c3)" "(a11, a12, a13, a14; a21, a22, a23, a24)" "(b1, b2)"
-        Power Shell> Optimal solution found.
-                     x = 3 * 1
-                         x1
-                         x2
-                         x3
+    EXAMPLE:
+    Power Shell> Simplex "(c1, c2, c3)" "(a11, a12, a13, a14; a21, a22, a23, a24)" "(b1, b2)"
+    Power Shell> Optimal solution found.
+    x = 3 * 1
+    x1
+    x2
+    x3
     This demo shows a user interface for this CLI based program.
     */
 
@@ -133,13 +135,15 @@ int main(int argc, int *argv[]) {
     test->low_level_array = a;
     test = number_mul_vector(3, 2, test);
     Matrix_print(test);
+    printf("\n");
 
     test = vector_add_vector(test, 1, 2);
-    printf("\n");
     Matrix_print(test);
+    printf("\n");
 
     test = pivot_Element_Trans(test, 2, 2);
     Matrix_print(test);
+    printf("\n");
 
     test = pivot_Element_Trans(test, 1, 1);
     Matrix_print(test);
